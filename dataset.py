5fa41cf8cc10f2c8519533a791ee7494cfdd744a
wtoth <wtoth@umich.edu>
2026-03-02 07:33:40 -0500

    Add cifar10 dataset

---
+++ dataset.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import torch 
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torchvision.transforms import v2
+from PIL import Image
+
+
+class Cifar10Dataset(Dataset):
+    def __init__(self, root_directory="", dataset="data/train.csv", transform=None):
+        self.root_directory = root_directory
+        self.image_directory = pd.read_csv(dataset)
+        self.transform = transform
+
+    def __len__(self) -> int:
+        return len(self.image_directory)
+    
+    def __getitem__(self, idx: int) -> tuple:
+        image_path = self.image_directory.iloc[idx, self.image_directory.columns.get_loc("filenames")]
+        label = self.image_directory.iloc[idx, self.image_directory.columns.get_loc("labels")]
+        data = torch.from_numpy(np.load(self.root_directory + image_path))
+
+        if self.transform:
+            data = self.transform(data)
+
+        return data, label
