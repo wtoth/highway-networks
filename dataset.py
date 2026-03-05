9bc207c61d370a84cd705bd56cea8d81c4077d3b
wtoth <wtoth@umich.edu>
2026-03-04 22:18:51 -0500

    Fix dataset dimension issues

--- dataset.py
+++ dataset.py
@@ -17,9 +17,17 @@ class Cifar10Dataset(Dataset):
     def __getitem__(self, idx: int) -> tuple:
         image_path = self.image_directory.iloc[idx, self.image_directory.columns.get_loc("filenames")]
         label = self.image_directory.iloc[idx, self.image_directory.columns.get_loc("labels")]
-        data = torch.from_numpy(np.load(self.root_directory + image_path)).reshape((32,32,3))
+        data = torch.from_numpy(np.load(self.root_directory + image_path)).reshape((3,32,32))
 
         if self.transform:
             data = self.transform(data)
 
         return data, label
+    
+
+# dataset sanity check
+import matplotlib.pyplot as plt
+def sanity_check():
+    data = torch.from_numpy(np.load("data/train/woodland_caribou_s_000999.npy")).reshape(3, 32, 32).permute(1,2,0)
+    plt.imshow(data.numpy())
+    plt.show() 
